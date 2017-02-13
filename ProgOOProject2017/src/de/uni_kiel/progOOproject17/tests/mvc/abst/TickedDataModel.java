03015a5034274cf52ffd98e729cf5ff9b40c2f10
YouMe2 <yaalex.devlab@gmail.com>
2017-02-13 23:54:55 +0100

    adds a short comment to the PongController

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/tests/mvc/abst/TickedDataModel.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/tests/mvc/abst/TickedDataModel.java
@@ -2,9 +2,6 @@ package de.uni_kiel.progOOproject17.tests.mvc.abst;
 
 public abstract class TickedDataModel extends AbstractDataModel{
 
-    public TickedDataModel() {
-	// TODO Auto-generated constructor stub
-    }
 
     public abstract void tick(long timestamp);
     
--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/tests/mvc/pong/PongController.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/tests/mvc/pong/PongController.java
@@ -6,10 +6,14 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.AbstractAction;
 
+import de.uni_kiel.progOOproject17.tests.mvc.abst.FramedIOView;
 import de.uni_kiel.progOOproject17.tests.mvc.abst.TickedController;
 import de.uni_kiel.progOOproject17.tests.mvc.abst.TickedDataModel;
 
 /**
+ * This is a sort of quick and dirty Pong Game based on the AbstarctMVC Classes:
+ * {@link FramedIOView}, {@link TickedController} and {@link TickedDataModel}.
+ * 
  * @author Yannik Eikmeier
  *
  */
@@ -17,34 +21,32 @@ public class PongController extends TickedController {
 
     private PongController c;
     private PongModel m;
-    
+
     public static void main(String[] args) {
 
 	PongView v = new PongView("MiniPong - YaAlex", 450, 300);
 	PongModel m = new PongModel(450, 300);
 	new PongController(v, m).start();
-	
 
     }
 
     public PongController(PongView view, TickedDataModel model) {
 	super(view, view, model);
 	c = this;
-	m = (PongModel)model;
-	
+	m = (PongModel) model;
+
 	view.addWindowListener(new WindowAdapter() {
-	    
+
 	    @Override
 	    public void windowClosing(WindowEvent e) {
-	        
+
 		c.stop();
 		System.exit(0);
-	        super.windowClosing(e);
+		super.windowClosing(e);
 	    }
-	    
+
 	});
-	
-	
+
 	standardIn.addKeyAction("pressed SPACE", new AbstractAction() {
 
 	    @Override
@@ -55,7 +57,7 @@ public class PongController extends TickedController {
 
 	    }
 	});
-	
+
 	standardIn.addKeyAction("released SPACE", new AbstractAction() {
 
 	    @Override
@@ -66,44 +68,44 @@ public class PongController extends TickedController {
 
 	    }
 	});
-	
+
 	standardIn.addKeyAction("pressed W", new AbstractAction() {
-	    
+
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		
+
 		m.bar1.moving = Bar.UP;
-		
+
 	    }
 	});
-	
+
 	standardIn.addKeyAction("released W", new AbstractAction() {
-	    
+
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		
+
 		m.bar1.moving = Bar.NONE;
-		
+
 	    }
 	});
-	
+
 	standardIn.addKeyAction("pressed S", new AbstractAction() {
-	    
+
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		
+
 		m.bar1.moving = Bar.DOWN;
-		
+
 	    }
 	});
-	
+
 	standardIn.addKeyAction("released S", new AbstractAction() {
-	    
+
 	    @Override
 	    public void actionPerformed(ActionEvent e) {
-		
+
 		m.bar1.moving = Bar.NONE;
-		
+
 	    }
 	});
 
