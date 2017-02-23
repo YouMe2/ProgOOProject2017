784914bb2d3fb6ff5d0af47ac9d390c7f45f2655
YouMe2 <yaalex.devlab@gmail.com>
2017-02-23 11:24:57 +0100

    adds a SidecrawlerMain.java

---
+++ ProgOOProject2017/src/main/SidecrawlerMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Yannik Eikmeier
+ * @since 23.02.2017
+ *
+ */
+public class SidecrawlerMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
