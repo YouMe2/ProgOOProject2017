40d1be71c7385c3318db15a40f3658661a8118d0
YouMe2 <yaalex.devlab@gmail.com>
2017-02-23 11:16:24 +0100

    cleans the master back to start

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/tests/mvc/abst/v/OutputView.java
+++
@@ -1,9 +0,0 @@
-package de.uni_kiel.progOOproject17.tests.mvc.abst.v;
-
-import de.uni_kiel.progOOproject17.tests.mvc.abst.m.AbstractDataModel;
-
-public interface OutputView {
-    
-    public void render(Viewable viewable);
-    
-}
