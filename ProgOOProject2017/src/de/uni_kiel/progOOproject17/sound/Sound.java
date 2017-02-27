35562e72643f633c6b44542905eb3fe80aa79654
Steffen <Steffen@172.25.42.20>
2017-02-27 18:18:11 +0100

    Started doing sound changes

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/sound/Sound.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/sound/Sound.java
@@ -5,50 +5,22 @@ import java.applet.AudioClip;
 
 public class Sound {
 
-	public static final Sound ping = new Sound("/pickup.wav"); // creates a sound
-																 // from
-																 // pickup.wav
-																 // file
+	public static final Sound PING = new Sound("pickup");
+	public static final Sound PLAYER_HURT = new Sound("playerhurt");
+	public static final Sound PLAYER_DEATH = new Sound("death");
+	public static final Sound MONSTER_HURT = new Sound("monsterhurt");
+	public static final Sound TEST = new Sound("test");
+	public static final Sound BOSS_DEATH = new Sound("bossdeath");
+	public static final Sound CRAFT = new Sound("craft");
 
-	public static final Sound playerHurt = new Sound("/playerhurt.wav"); // creates
-																		 // a
-																		 // sound
-																		 // from
-																		 // playerhurt.wav
-																		 // file
-	public static final Sound playerDeath = new Sound("/death.wav"); // creates a
-																	 // sound
-																	 // from
-																	 // death.wav
-																	 // file
-	public static final Sound monsterHurt = new Sound("/monsterhurt.wav"); // creates
-																			 // a
-																			 // sound
-																			 // from
-																			 // monsterhurt.wav
-																			 // file
-	public static final Sound test = new Sound("/test.wav"); // creates a sound
-															 // from test.wav
-															 // file
-	public static final Sound bossdeath = new Sound("/bossdeath.wav"); // creates
-																		 // a
-																		 // sound
-																		 // from
-																		 // bossdeath.wav
-																		 // file
-	public static final Sound craft = new Sound("/craft.wav"); // creates a sound
-																 // from
-																 // craft.wav
-																 // file
 	private AudioClip clip; // Creates a audio clip to be played
 
 	private Sound(String name) {
+		// clip = Resources.getInstance().getSound(name);
 		try {
 
-			//tries to load the audio clip from the name you gave above.
+			// tries to load the audio clip from the name you gave above.
 			clip = Applet.newAudioClip(Sound.class.getResource(name));
-			
-		
 
 		} catch (Throwable e) {
 			e.printStackTrace(); // else it will throw an error
