aea12f2034a206c8ecc86c8bf2054291862f3e9d
Steffen <Steffen@172.25.42.20>
2017-02-27 17:10:27 +0100

    Added resource manager

--- ProgOOProject2017/src/main/SidecrawlerMain.java
+++ ProgOOProject2017/src/main/SidecrawlerMain.java
@@ -6,6 +6,7 @@ package main;
 import de.uni_kiel.progOOproject17.controller.PLController;
 import de.uni_kiel.progOOproject17.model.PLGameModel;
 import de.uni_kiel.progOOproject17.view.PLDektopView;
+import de.uni_kiel.progOOproject17.view.abs.Resources;
 
 /**
  * @author Yannik Eikmeier
@@ -17,6 +18,8 @@ public class SidecrawlerMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Resources.getInstance().init();
 
 		// TODO set Title
 		new PLController(new PLDektopView("Sidecrawler Test Title"),
