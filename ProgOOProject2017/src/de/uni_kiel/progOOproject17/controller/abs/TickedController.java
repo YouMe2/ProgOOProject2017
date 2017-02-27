03ad2ec6869ebfcd9b59b317c1ad696cf1e50067
YouMe2 <yaalex.devlab@gmail.com>
2017-02-27 12:10:12 +0100

    clean and new start

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/controller/abs/TickedController.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/controller/abs/TickedController.java
@@ -5,41 +5,27 @@ import de.uni_kiel.progOOproject17.model.abs.TickedDataModel;
 import de.uni_kiel.progOOproject17.view.abs.InputView;
 import de.uni_kiel.progOOproject17.view.abs.OutputView;
 
-<<<<<<< HEAD
-public abstract class TickedController extends AbstractController
-		implements Runnable, Ticked {
-=======
+
 public abstract class TickedController extends AbstractController implements Runnable, Ticked {
->>>>>>> refs/heads/sidecrawlerBaseEngine
+
 
 	private Thread thread;
-<<<<<<< HEAD
-	private boolean runnig = false;
-=======
+
 	private boolean running = false;
->>>>>>> refs/heads/sidecrawlerBaseEngine
+	
 	private int ticklength = 20;// ms = 1000/50 -> 50 ticks/sec
 	private int fps = 0;
 	private int tps = 0;
 	private long gametime = 0;
 	private boolean printTpsFps = false;
 
-<<<<<<< HEAD
-	public TickedController(OutputView out, InputView in, TickedDataModel model,
-			int ticklength) {
-=======
+
 	public TickedController(OutputView out, InputView in, TickedDataModel model, int ticklength) {
->>>>>>> refs/heads/sidecrawlerBaseEngine
 		super(out, in, model);
 		this.ticklength = ticklength;
 	}
 
-<<<<<<< HEAD
-	public TickedController(OutputView out, InputView in,
-			TickedDataModel model) {
-=======
 	public TickedController(OutputView out, InputView in, TickedDataModel model) {
->>>>>>> refs/heads/sidecrawlerBaseEngine
 		super(out, in, model);
 	}
 
@@ -55,63 +41,7 @@ public abstract class TickedController extends AbstractController implements Run
 		int frames = 0;
 		int ticks = 0;
 
-<<<<<<< HEAD
-		while (runnig) {
 
-			long newTime = System.currentTimeMillis();
-			long frameTime = newTime - oldTime;
-			oldTime = newTime;
-
-			secTimer += frameTime;
-			timeAccumulator += frameTime; // this is how much time the game
-			// has to calculate ticks for
-
-			if (timeAccumulator >= ticklength) {
-
-				while (timeAccumulator >= ticklength) {
-					// System.out.println("tick");
-					ticks++;
-					tick(gametime);
-					timeAccumulator -= ticklength;
-					gametime += ticklength;
-				}
-				// System.out.println("render");
-				frames++;
-				renderAllViews();
-
-			}
-
-			if (secTimer >= 1000) {
-				fps = frames;
-				tps = ticks;
-				secTimer = 0;
-				frames = 0;
-				ticks = 0;
-				if (printTpsFps)
-					System.out.println(
-							"Running at: " + tps + " tps and " + fps + " fps.");
-
-			}
-
-		}
-	}
-
-	public void start() {
-		if (thread == null) {
-			thread = new Thread(this);
-			runnig = true;
-			gametime = 0;
-			thread.start();
-		}
-	}
-
-	public void stop() {
-		runnig = false;
-		thread = null;
-	}
-
-	@Override
-=======
 		while (running) {
 
 			long newTime = System.currentTimeMillis();
@@ -166,7 +96,6 @@ public abstract class TickedController extends AbstractController implements Run
 		thread = null;
 	}
 
->>>>>>> refs/heads/sidecrawlerBaseEngine
 	public void tick(long timestamp) {
 		getModel().tick(timestamp);
 	}
