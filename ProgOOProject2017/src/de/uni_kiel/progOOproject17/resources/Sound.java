86df571ded728cb275ebdd506ba055714133cb06
Steffen <Steffen@172.25.42.20>
2017-03-02 13:58:41 +0100

    Added game properties

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/resources/Sound.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/resources/Sound.java
@@ -4,24 +4,24 @@ import javax.sound.sampled.Clip;
 
 public class Sound {
 
-    // public static final Sound PING = new Sound("pickup");
-    // public static final Sound PLAYER_HURT = new Sound("playerhurt");
-    // public static final Sound PLAYER_DEATH = new Sound("death");
-    // public static final Sound MONSTER_HURT = new Sound("monsterhurt");
-    // public static final Sound TEST = new Sound("test");
-    // public static final Sound BOSS_DEATH = new Sound("bossdeath");
-    // public static final Sound CRAFT = new Sound("craft");
+	// public static final Sound PING = new Sound("pickup");
+	// public static final Sound PLAYER_HURT = new Sound("playerhurt");
+	// public static final Sound PLAYER_DEATH = new Sound("death");
+	// public static final Sound MONSTER_HURT = new Sound("monsterhurt");
+	// public static final Sound TEST = new Sound("test");
+	// public static final Sound BOSS_DEATH = new Sound("bossdeath");
+	// public static final Sound CRAFT = new Sound("craft");
 
-    private Clip sound;
+	private Clip sound;
 
-    Sound(Clip sound) {
-	this.sound = sound;
-    }
+	Sound(Clip sound) {
+		this.sound = sound;
+	}
 
-    public void play() {
-	if (sound.isActive())
-	    sound.stop();
-	sound.setFramePosition(0);
-	sound.start();
-    }
+	public void play() {
+		if (sound.isActive())
+			sound.stop();
+		sound.setFramePosition(0);
+		sound.start();
+	}
 }
