04e5eb2291ea00f846129922f2397a9e479368b9
Steffen <Steffen@192.168.178.35>
2017-03-03 18:28:48 +0100

    Improved level gen readability

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/model/levelgen/Obstacle.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/model/levelgen/Obstacle.java
@@ -10,32 +10,19 @@ import java.awt.Point;
 public enum Obstacle {
 
 	SINGLE(100, (x, e, c) -> {
-		// System.out.println("single obstacle for " + x);
-		Distance standardVelocity = new Distance(-5, 0);
-
+		Distance velocity = new Distance(-5, 0);
 		Enemy e1 = new Enemy("enemy", new Point(x, 200), e, c);
-		e1.setVelocity(standardVelocity);
-
-		// Enemy e2 = new Enemy("enemy", new Point(300, 300), e, c);
-		// e2.setVelocity(standardVelocity);
-		//
-		// Block b = new Block("floor", new Point(300, 300), e, c);
-
+		e1.setVelocity(velocity);
 		return new GameElement[] { e1/* , e2, b */ };
 	}), DOUBLE(180, (x, e, c) -> {
-		// System.out.println("double obstacle for " + x);
 		return new GameElement[] {};
 	}), TRIPLE(250, (x, e, c) -> {
-		// System.out.println("triple obstacle for " + x);
 		return new GameElement[] {};
 	}), HOVERING(100, (x, e, c) -> {
-		// System.out.println("hovering obstacle for " + x);
 		return new GameElement[] {};
 	}), DOUBLE_HOVERING(180, (x, e, c) -> {
-		// System.out.println("double hovering obstacle for " + x);
 		return new GameElement[] {};
 	}), TRIPLE_HOVERING(250, (x, e, c) -> {
-		// System.out.println("triple hovering obstacle for " + x);
 		return new GameElement[] {};
 	});
 
