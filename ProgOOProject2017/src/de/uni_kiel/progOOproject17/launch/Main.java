c8d1b4a959430913335e1374e98a0e650bf8d7e4
Steffen <Steffen@192.168.2.183>
2017-03-07 09:29:16 +0100

    Improved image inclusion in documentation of main type

--- ProgOOProject2017/src/de/uni_kiel/progOOproject17/launch/Main.java
+++ ProgOOProject2017/src/de/uni_kiel/progOOproject17/launch/Main.java
@@ -8,7 +8,8 @@ import de.uni_kiel.progOOproject17.view.PLDektopView;
 /**
  * TODO description of class hierarchy
  * <p>
- * <img src="model_class_hierarchy.png" title="Class hierarchy of the model">
+ * <img src="model_class_hierarchy.png" title="Class hierarchy of the model" alt
+ * ="Class hierarchy of the model" width="800" height="355">
  *
  */
 public class Main {
